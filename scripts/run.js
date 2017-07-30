c6b8412aca0a6d8131a5dbbbcedac1f798c9a011
APalumbo97 <ajpalumbo97@gmail.com>
2017-07-30 12:53:00 -0400

    Added stubbed out Timer class and ability to switch from timer to stopwatch and back.

--- scripts/run.js
+++ scripts/run.js
@@ -4,10 +4,49 @@
  * Description: Sets up the buttons for the stopwatch and timer.
  */
 
+var timer;
+var stopwatch;
+
+/**
+ * Sets up the timer controls.
+ */
+function initializeTimer() {
+    // TODO
+}
+
+/**
+ * Removes the timer HTML elements from the page.
+ */
+function removeTimer() {
+    // TODO
+    timer = null;
+}
+
+/**
+ * Transitions from the stopwatch functionality to the timer functionality.
+ */
+function switchToTimer() {
+    removeStopwatch();
+    initializeTimer();
+    timer = new Timer();
+}
+
 /**
  * Sets up the buttons to control the stopwatch.
  */
-function initialize() {
+function initializeStopwatch() {
+    var stopwatchDiv = document.createElement("div");
+    stopwatchDiv.setAttribute("id", "stopwatch");
+    document.body.appendChild(stopwatchDiv);
+
+    var buttonsDiv = document.createElement("div");
+    buttonsDiv.setAttribute("id", "buttons");
+    document.body.appendChild(buttonsDiv);
+
+    var lapsUl = document.createElement("ul");
+    lapsUl.setAttribute("id", "laps");
+    document.body.appendChild(lapsUl);
+
     var startBtn = document.createElement("button");
     startBtn.innerHTML = "Start";
     startBtn.setAttribute("onClick", "stopwatch.start();");
@@ -29,6 +68,34 @@ function initialize() {
     document.getElementById("buttons").appendChild(lapBtn);
 }
 
-var stopwatch = new Stopwatch(document.querySelector(".stopwatch"), document.querySelector("#laps"));
+/**
+ * Removes the stopwatch HTML elements from the page.
+ */
+function removeStopwatch() {
+    stopwatch.reset();
+    document.body.removeChild(document.getElementById("stopwatch"));
+    document.body.removeChild(document.getElementById("buttons"));
+    document.body.removeChild(document.getElementById("laps"));
+    stopwatch = null;
+}
 
-initialize();
+/**
+ * Transitions from the timer functionality to the stopwatch functionality.
+ */
+function switchToStopwatch() {
+    removeTimer();
+    initializeStopwatch();
+    stopwatch = new Stopwatch(document.querySelector("#stopwatch"), document.querySelector("#laps"));
+}
+
+/**
+ * Switches to either the timer or stopwatch.
+ * @param checkbox - the HTML element for the switch
+ */
+function checkBoxClick(checkbox) {
+    if (checkbox.checked) {
+        switchToStopwatch();
+    } else {
+        switchToTimer();
+    }
+}
---
+++ scripts/timer.js
@@ -0,0 +1,11 @@
+/**
+ * Author: Anthony Palumbo
+ * Date: 7-30-2017
+ * Description: Class for modeling a timer's functionality.
+ */
+
+class Timer {
+    constructor() {
+        document.body.style.backgroundColor = "red";
+    }
+}
